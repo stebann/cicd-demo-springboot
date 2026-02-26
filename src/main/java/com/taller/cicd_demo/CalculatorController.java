5bc47091d217bede118f8b5bca0be4f2a52c579e
Esteban Ramirez <estebanraul313@gmail.com>
2026-02-25 23:34:13 -0500

    fix: restore correct sum method

--- src/main/java/com/taller/cicd_demo/CalculatorController.java
+++ src/main/java/com/taller/cicd_demo/CalculatorController.java
@@ -11,7 +11,7 @@ public class CalculatorController {
 
     @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
-        return a * b; // BUG: debería ser a + b
+        return a + b;
     }
 
     @GetMapping("/multiply")
